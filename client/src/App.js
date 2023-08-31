5d9e6c855e4bb206e11066d43ade7f9367ed4d3f
retr0 <uzairahmednasir@outlook.com>
2023-08-31 19:08:54 +0500

    react router

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,11 @@ function App() {
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <Routes>
-            <Route path="/" element={<LoginPage />} />
+            {/* <Route path="/" element={<LoginPage />} /> */}
+            <Route
+              path="/"
+              element={!isAuth ? <LoginPage /> : <Navigate to="/home" />}
+            />
             <Route
               path="/home"
               element={isAuth ? <HomePage /> : <Navigate to="/" />}
