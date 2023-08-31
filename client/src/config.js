4ea6ac6913ff597dd76fc78bae61086759626e7b
retr0 <uzairahmednasir@outlook.com>
2023-08-31 18:09:08 +0500

    baseUrl update

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "https://urchin-app-v2nci.ondigitalocean.app/";
+export const API_BASE_URL = "https://urchin-app-v2nci.ondigitalocean.app";
 // export const API_BASE_URL = "http://localhost:3001";
